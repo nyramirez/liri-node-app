76c2e5dfdefe7b5ada1eacaa37b8da9b998366e1
Nefas <nyramirez@gmail.com>
2018-10-17 18:54:16 -0500

    creating files and adding gitignore file

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.bandsInTown = {
+  id: process.env.BANDSINTOWN_ID
+};
+
+exports.omdb = {
+  id: process.env.OMDb_ID
+};
