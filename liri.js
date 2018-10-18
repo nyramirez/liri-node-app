74491d36b5b974f855482c7a9d40defc57a3feb3
Nefas <nyramirez@gmail.com>
2018-10-18 09:58:16 -0500

    moving files to folder

--- liri.js
+++ liri.js
@@ -1,1 +1,6 @@
 require("dotenv").config();
+
+console.log(process.env.SPOTIFY_ID);
+console.log(process.env.SPOTIFY_SECRET);
+console.log(process.env.BANDSINTOWN_ID);
+console.log(process.env.OMDb_ID);
